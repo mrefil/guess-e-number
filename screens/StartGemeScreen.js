778ac7def613c4bfb231da45777cde09e3be3597
Mustafa E <61510151+mrefil@users.noreply.github.com>
2021-10-03 23:28:36 +0300

    move Card in new component

--- screens/StartGemeScreen.js
+++ screens/StartGemeScreen.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import Card from '../components/Card';
 
 const StartGameScreen = props => {
     <View style={styles.screen}>
         <Text style={styles.title}>Start a New Game</Text>
-        <View style={styles.inputContainer}>
+        <Card style={styles.inputContainer}>
             <Text>Select a Number</Text>
             <TextInput />
             <View style={styles.buttonContainer}>
                 <Button title="Reset" onPress={() => {}} />
                 <Button title="Confirm" />
             </View>
-        </View>
+        </Card>
     </View>
 }
 
@@ -28,15 +29,7 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center',
-        shadowColor: '#000000',
-        shadowOffset: {width: 20, height: 2},
-        shadowRadius: 6,
-        shadowOpacity: 0.26,
-        backgroundColor: 'white',
-        elevation: 8,
-        padding: 20,
-        borderRadius: 10
+        alignItems: 'center'
     },
     buttonContainer: {
         flexDirection: 'row',
